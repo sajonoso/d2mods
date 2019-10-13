6d9016a95fc2398ab5c4670a0bf775cf711b0c8e
Sajon Oso <sajonoso@users.noreply.github.com>
2019-10-13 18:47:11 +1100

    update build to bundle javascript and images in separate files

--- build.js
+++ build.js
@@ -1,32 +1,83 @@
-// require = typeof (require) === 'function' ? require : function (file) {
-//   var fh = (new ActiveXObject('Scripting.FileSystemObject')).OpenTextFile(file + '.js', 1);
-//   var content = fh.ReadAll(); fh.close(); eval(content); return fs;
-// }
-// require('./fsxcl');
 const fs = require('fs')
+const os = require('os')
 const process = require('process')
+const spawn = require('child_process').spawn
+const crypto = require('crypto')
+
 
 const BASE_FOLDER = 'src'
 const BASE_FILE = 'index.htm'
+const OUTPUT_FOLDER = 'docs'
 const BUNDLE_FILE = '../docs/index.html' // relative to BASE_FOLDER
+const SCRIPT_BUNDLE_FILE = 'bundled.js'
+const CSS_BUNDLE_FILE = 'bundled.css'
 
+const ImageList = []
+const ScriptList = []
+const CSSList = []
+const NODE_VERSION = Number(process.version.match(/^v(\d+\.\d+)/)[1])
+
+
+function sha1hash(plainstring) {
+  return crypto.createHash('sha1').update(plainstring).digest('hex')
+}
+
+// abstract file copy operation
+function CopyFileSync(source, destination) {
+  let sourceFile = source
+  let destinationFile = destination
+
+  if (os.platform() === 'win32') {
+    sourceFile = source.replace(/\//g, '\\')
+    destinationFile = destination.replace(/\//g, '\\')
+  }
+
+  if (NODE_VERSION > 8.5) {
+    return fs.copyFileSync(source, destination, (err) => {
+      if (err) throw err;
+      console.log('source.txt was copied to destination.txt');
+    });
+  } else {
+    if (os.platform() === 'win32') {
+      spawn('cmd.exe', ['/c', 'copy', '/b', sourceFile, destinationFile])
+    } else {
+      throw Error('Node version less than 8.5 does not support file copy')
+    }
+  }
+}
 
 function CheckImageTag(line) {
   const isImageTag = line.match(/<img(.*?)src='(.*?).png'>/)
   if (isImageTag) {
     const imgFile = isImageTag[2] + '.png';
+    const imgAttributes = isImageTag[1] ? isImageTag[1] : ''
+    const png_imageid = sha1hash(imgFile)
+
     if (fs.existsSync(imgFile)) {
-      const imgFileBinary = fs.readFileSync(imgFile, 'base64');
-      return '<img ' + isImageTag[1] + 'src="data:image/png;base64,' + imgFileBinary + '">';
+      if (ImageList.indexOf(imgFile) < 0) {
+        ImageList.push(imgFile)
+        // const imgFileBinary = fs.readFileSync(imgFile, 'base64');
+        CopyFileSync('./' + imgFile, '../' + OUTPUT_FOLDER + '/' + imgFile)
+      }
+      return '<img id="' + png_imageid + '" ' + imgAttributes + 'src="' + imgFile + '">';
+      // return '<img ' + isImageTag[1] + 'src="data:image/png;base64,' + imgFileBinary + '">';
     }
   }
 
   const isJpgImageTag = line.match(/<img(.*?)src='(.*?).jpg'>/)
   if (isJpgImageTag) {
     const imgFile = isJpgImageTag[2] + '.jpg';
+    const imgAttributes = isJpgImageTag[1] ? isJpgImageTag[1] : ''
+    const jpg_imageid = sha1hash(imgFile)
+
     if (fs.existsSync(imgFile)) {
-      const imgFileBinary = fs.readFileSync(imgFile, 'base64');
-      return '<img ' + isJpgImageTag[1] + 'src="data:image/jpg;base64,' + imgFileBinary + '">';
+      if (ImageList.indexOf(imgFile) < 0) {
+        ImageList.push(imgFile)
+        // const imgFileBinary = fs.readFileSync(imgFile, 'base64');
+        CopyFileSync('./' + imgFile, '../' + OUTPUT_FOLDER + '/' + imgFile)
+      }
+      return '<img id="' + jpg_imageid + '" ' + imgAttributes + 'src="' + imgFile + '">';
+      // return '<img ' + isJpgImageTag[1] + 'src="data:image/jpg;base64,' + imgFileBinary + '">';
     }
   }
 
@@ -37,21 +88,54 @@ function CheckScriptTag(line) {
   var isScriptTag = line.match(/<script src='(.*?).js'><\/script>/)
   if (isScriptTag) {
     const scriptFile = isScriptTag[1] + '.js';
+    const scriptid = sha1hash(scriptFile)
+
     if (fs.existsSync(scriptFile)) {
-      const scriptFileContent = fs.readFileSync(scriptFile, 'utf-8');
-      return '<script>\n' + scriptFileContent + '\n</script>';
+      // uncomment next two lines to bundle scripts in html file and comment out remainder of block
+      // const scriptFileContent = fs.readFileSync(scriptFile, 'utf-8');
+      // return '\n<script>\n' + scriptFileContent + '\n</script>\n';
+
+      if (ScriptList.indexOf(scriptFile) < 0) {
+        ScriptList.push(scriptFile)
+        const scriptFileContent = fs.readFileSync(scriptFile, 'utf-8');
+        const bundleFile = '../' + OUTPUT_FOLDER + '/' + SCRIPT_BUNDLE_FILE
+        const newBundleContent = '// #### ' + scriptFile + '\n' + scriptFileContent + '\n'
+        if (ScriptList.length === 1) {
+          fs.writeFileSync(bundleFile, newBundleContent)
+        } else {
+          fs.appendFileSync(bundleFile, newBundleContent)
+        }
+      }
+      if (ScriptList.length === 1) return '<script src="' + SCRIPT_BUNDLE_FILE + '"></script>\n'
+      return '<!-- ' + scriptFile + ' -->'
     }
   }
   return '';
 }
 
 function CheckCSSTag(line) {
-  var isScriptTag = line.match(/<link rel='stylesheet' href='(.*?).css'>/)
-  if (isScriptTag) {
-    const scriptFile = isScriptTag[1] + '.css';
-    if (fs.existsSync(scriptFile)) {
-      const scriptFileContent = fs.readFileSync(scriptFile, 'utf-8');
-      return '<style>\n' + scriptFileContent + '\n</style>';
+  var isCSSTag = line.match(/<link rel='stylesheet' href='(.*?).css'>/)
+  if (isCSSTag) {
+    const cssFile = isCSSTag[1] + '.css';
+    const cssFileid = sha1hash(cssFile)
+
+    if (fs.existsSync(cssFile)) {
+      const cssFileContent = fs.readFileSync(cssFile, 'utf-8');
+      return '<style>\n' + cssFileContent + '\n</style>';
+
+      // if (CSSList.indexOf(cssFile) < 0) {
+      //   CSSList.push(cssFile)
+      //   const cssFileContent = fs.readFileSync(cssFile, 'utf-8');
+      //   const bundleFile = '../' + OUTPUT_FOLDER + '/' + CSS_BUNDLE_FILE
+      //   const newBundleContent = '/* #### ' + cssFile + ' */\n' + cssFileContent + '\n'
+      //   if (CSSList.length === 1) {
+      //     fs.writeFileSync(bundleFile, newBundleContent)
+      //   } else {
+      //     fs.appendFileSync(bundleFile, newBundleContent)
+      //   }
+      // }
+      // if (CSSList.length === 1) return '<link rel="stylesheet" href="' + CSS_BUNDLE_FILE + '">\n'
+      // return '<!-- ' + cssFile + ' -->'
     }
   }
   return '';
@@ -113,8 +197,7 @@ function DirList(dir, filelist) {
   files.forEach(function (file) {
     if (fs.statSync(dir + '/' + file).isDirectory()) {
       filelist = DirList(dir + '/' + file, filelist)
-    }
-    else {
+    } else {
       filelist.push(dir + '/' + file)
     }
   })
@@ -131,7 +214,14 @@ function CompressFolder(fileList, nextFunction) {
     zip.file(file, fileContent)
   })
 
-  zip.generateAsync({ type: "base64", compression: "DEFLATE", compressionOptions: { level: 9 } })
+  const zipOptions = {
+    type: "base64",
+    compression: "DEFLATE",
+    compressionOptions: {
+      level: 9
+    }
+  }
+  zip.generateAsync(zipOptions)
     .then(function (base64Zip) {
       const fdOut = fs.openSync(OUTPUT_FILE + '.js', 'w');
       fs.writeSync(fdOut, 'const DATA_ZIP="' + base64Zip + '";');
@@ -145,8 +235,8 @@ function CompressFolder(fileList, nextFunction) {
 process.chdir(BASE_FOLDER)
 const fileList = DirList(INPUT_FOLDER)
 
-CompressFolder(fileList, function() {
+CompressFolder(fileList, function () {
+  if (!fs.existsSync('../' + OUTPUT_FOLDER)) fs.mkdirSync('../' + OUTPUT_FOLDER)
+  if (!fs.existsSync('../' + OUTPUT_FOLDER + '/images')) fs.mkdirSync('../' + OUTPUT_FOLDER + '/images')
   BuildBundle();
 })
-
-
