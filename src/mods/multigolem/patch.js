271540860775fdd4d6a77ca8575e96594b6f47ec
Sajon Oso <sajonoso@users.noreply.github.com>
2021-08-28 23:57:41 +1000

    update multi-golem mod with dynamic id in pettype.txt

---
+++ src/mods/multigolem/patch.js
@@ -0,0 +1,14 @@
+modder.currentPatch = function applyPatch(mod, targetFiles) {
+    var fileLines = targetFiles['pettype.txt']
+    var columns = fileLines[0].split('\t')
+
+    var maxID = modder.get_max_column(fileLines, columns, 'idx');
+
+    var additions = [
+        "claygolem\t" + (maxID + 1) + "\t\t0\t1\t\t1\t1\t1\tStrUI0\t1\t3\tearthgolumicon\t289\tearthgolumicon\t\t\t\t\t\t\t0",
+        "bloodgolem\t" + (maxID + 2) + "\t\t0\t1\t\t1\t1\t1\tStrUI0\t1\t3\tbloodgolumicon\t290\tbloodgolumicon\t\t\t\t\t\t\t0",
+        "firegolem\t" + (maxID + 3) + "\t\t0\t1\t\t1\t1\t1\tStrUI0\t1\t3\tfiregolumicon\t292\tfiregolumicon\t\t\t\t\t\t\t0",
+    ]
+
+    targetFiles['pettype.txt'] = fileLines.concat(additions)
+}
